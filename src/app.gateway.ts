67a3d14f7da963035d805fa2c68a840124091fc2
martin <martinfrontend@gmail.com>
2021-02-02 18:02:19 +0800

    no message

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -9,22 +9,29 @@ import { Socket, Server } from 'socket.io';
 @WebSocketGateway()
 export class AppGateway {
   @WebSocketServer() server: Server;
-  users = 0;
+  users = {};
   record = {
     userCount: 0,
     messages: [],
   };
+  name = [];
   private logger: Logger = new Logger('AppGateway');
 
   @SubscribeMessage('msgToServer')
   handleMessage(client: Socket, payload: string): void {
-    console.log(payload);
     this.record.messages.push(payload);
     this.server.emit('msgToClient', this.record);
   }
 
   @SubscribeMessage('login')
   handleLogin(client: Socket, payload: string): void {
+    // this.name = payload;
+    this.users[client.id] = payload;
+    this.record.messages.push({
+      message: '已連線',
+      name: payload,
+      type: 'system',
+    });
     this.server.emit('msgToClient', this.record);
   }
 
@@ -33,12 +40,28 @@ export class AppGateway {
     this.server.emit('userCount', payload);
   }
 
+  handleLogout(client: Socket, payload: string): void {
+    // this.name = payload;
+    this.record.messages.push({
+      message: '已下線',
+      name: payload,
+      type: 'system',
+    });
+    this.server.emit('msgToClient', this.record);
+  }
+
   afterInit(server: Server) {
     this.logger.log('Init');
   }
 
   handleDisconnect(client: Socket) {
-    this.users--;
+    console.log(this.users);
+    this.record.messages.push({
+      message: '已離線',
+      name: this.users[client.id],
+      type: 'system',
+    });
+    this.server.emit('msgToClient', this.record);
     this.logger.log(`Client disconnected: ${client.id}, ${this.users}`);
   }
 
