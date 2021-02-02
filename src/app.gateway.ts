d8bb6972b51ab85c6171b7f1120ba37efab92770
martin <martinfrontend@gmail.com>
2021-02-02 17:26:17 +0800

    no message

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -10,12 +10,27 @@ import { Socket, Server } from 'socket.io';
 export class AppGateway {
   @WebSocketServer() server: Server;
   users = 0;
+  record = {
+    userCount: 0,
+    messages: [],
+  };
   private logger: Logger = new Logger('AppGateway');
 
   @SubscribeMessage('msgToServer')
   handleMessage(client: Socket, payload: string): void {
     console.log(payload);
-    this.server.emit('msgToClient', payload);
+    this.record.messages.push(payload);
+    this.server.emit('msgToClient', this.record);
+  }
+
+  @SubscribeMessage('login')
+  handleLogin(client: Socket, payload: string): void {
+    this.server.emit('msgToClient', this.record);
+  }
+
+  @SubscribeMessage('userCount')
+  handleUserCount(client: Socket, payload: string): void {
+    this.server.emit('userCount', payload);
   }
 
   afterInit(server: Server) {
@@ -24,12 +39,11 @@ export class AppGateway {
 
   handleDisconnect(client: Socket) {
     this.users--;
-    this.logger.log(`Client disconnected: ${client.id}`);
+    this.logger.log(`Client disconnected: ${client.id}, ${this.users}`);
   }
 
   handleConnection(client: Socket, ...args: any[]) {
-    this.users++;
     // console.log(client);
-    this.logger.log(`Client connected: ${client.id}`);
+    this.logger.log(`Client connected: ${client.id}, ${this.users}`);
   }
 }
